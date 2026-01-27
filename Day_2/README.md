635091f146a7adb73be617dfc13f1aa0bce07b0e
Vikram K M <vikramkm12345@gmail.com>
2026-01-27 07:28:02 +0530

    Day-2 Completed

---
+++ Day_2/README.md
@@ -0,0 +1,44 @@
+Day-2:
+    Date: 26/JAN/2026
+    Phase:0
+
+    Concepts Covered- 
+        ✅ Function Design
+                Parameters vs arguments
+                Positional vs keyword arguments
+                Default arguments (⚠️ mutable default trap)
+                Return values vs side effects
+                Pure vs impure functions
+        ✅ Error Handling
+                try / except / else / finally
+                Catching specific exceptions
+                When to re-raise vs handle
+                raise vs logging.exception
+        ✅ Itteration Patterns
+                enumerate
+                zip
+                Loop + condition + break
+                Early exit patterns
+
+    Problems Solved(Same as Day-1 but by retruning each result):
+        ✅ LIST PROBLEMS
+        Remove duplicates from a list
+        Find the second largest number
+        Reverse a list without slicing
+        Rotate a list k times
+        Flatten a nested list
+
+        ✅ SET PROBLEMS
+        Find common elements between two lists
+        Find elements present in only one list
+        Check if one list is a subset of another
+
+        ✅ DICTIONARY PROBLEMS
+        Count frequency of elements
+        Find first non-repeating element
+        Group elements by frequency
+        Merge two dictionaries (sum values if keys overlap)
+
+
+            
+                
---
+++ Day_2/src/List-Set-Dict problems.py
@@ -0,0 +1,125 @@
+import logging
+from collections import Counter
+
+logging.basicConfig(level=logging.INFO,format='%(asctime)s, - %(levelname)s - %(message)s')
+
+rotation_count=4
+day1_list = [5, 3, 8, 3, 9, 1, 5, 7, 2, 8]
+numbers_list = [12, 45, 2, 67, 45, 89, 34, 67, 23]
+day1_nested_list = [[1, 2, [3, 4]], [5, 6], [7, [8, 9]]]
+list_a = [1, 2, 3, 4, 5, 6, 7]
+list_b = [5, 6, 7, 8, 9]
+freq_list = ['apple', 'banana', 'apple', 'orange', 'banana', 'apple', 'kiwi', 'kiwi', 'mango']
+dict1 = {'a':3, 'b':2}
+dict2 = {'b':1, 'c':5}
+
+
+#Remove duplicates from a list
+# Time Complexity: O(n)
+# Space Complexity: O(n)
+def remove_duplicates(lst_obj):
+    return list(dict.fromkeys(lst_obj).keys())
+   
+#Find the second largest number
+# Time Complexity: O(n)
+# Space Complexity: O(n)
+def second_largest(lst_obj):
+    unq=list(set(lst_obj))
+    if len(unq)==1:
+        return ''
+    else:
+        return (sorted(unq)[-2])
+
+#Reverse a list without slicing
+# Time Complexity: O(n)
+# Space Complexity: O(n)
+def reverse_list(lst_obj):
+    return [lst_obj[i] for i in range(len(lst_obj)-1,-1,-1)]
+
+#Rotate a list k times
+# Time Complexity: O(n)
+# Space Complexity: O(n)
+def rotate_list(lst_obj,k):
+    k=k%len(lst_obj) if k>len(lst_obj) else k
+    return lst_obj[-k:]+lst_obj[:-k]
+    
+    
+#Flatten a nested list
+# Time Complexity: O(n)
+# Space Complexity: O(n)
+def flatten_list(lst_obj):
+    for i in lst_obj:
+        (yield from flatten_list(i)) if isinstance(i,list) else (yield i)
+
+#Set Problems:
+        
+#Find common elements between two lists
+# Time Complexity: O(n)
+# Space Complexity: O(n)
+def common_elements(lst_obj1,lst_obj2):
+    return set(lst_obj1).intersection(set(lst_obj2))
+
+#Find elements present in only one list
+# Time Complexity: O(n)
+# Space Complexity: O(n)
+def onlyin_oneset(lst_obj1,lst_obj2):
+    return set(lst_obj1)-(set(lst_obj2))
+
+#Check if one list is a subset of another
+# Time Complexity: O(n)
+# Space Complexity: O(n)
+def subset(lst_obj1,lst_obj2):
+    return set(lst_obj1).issubset(set(lst_obj2))
+
+#Count frequency of elements
+# Time Complexity: O(n)
+# Space Complexity: O(n)
+def dict_frequency(freq_list):
+    return dict(Counter(freq_list))
+
+#Find first non-repeating element
+# Time Complexity: O(n)
+# Space Complexity: O(n)
+def dict_first_unique(freq_list):
+    unq=dict(Counter(freq_list))
+    for k,v in unq.items():
+        if v==1:
+            return k
+        
+
+#Group elements by frequency
+# Time Complexity: O(n)
+# Space Complexity: O(n)
+def dict_freq_grp(freq_list):
+    d={}
+    unq=dict(Counter(freq_list))
+    for k,v in unq.items():
+        d.setdefault(v,[]).append(k)
+    return d
+
+#Merge two dictionaries (sum values if keys overlap)
+# Time Complexity: O(n)
+# Space Complexity: O(n)
+def dict_merge(dict1,dict2):
+    return {x:dict1.get(x,0)+dict2.get(x,0) for x in set(dict1)|set(dict2)}
+    
+def main():
+    try:
+        logging.info(f'1.List After Removing Duplicates: {remove_duplicates(day1_list)}') 
+        logging.info(f'2.There is NO second largest element') if second_largest(numbers_list)=='' else logging.info(f'2.Second Largest: {second_largest(numbers_list)}')
+        logging.info(f'3.Reversed List: {reverse_list(day1_list)}')
+        logging.info(f'4.Rotated List {rotation_count} times: {rotate_list(numbers_list,rotation_count)}')
+        logging.info(f'5.Flattened List: {list(flatten_list(day1_nested_list))}')
+        logging.info(f'6.Common Elements: {common_elements(list_a,list_b)}')
+        logging.info(f'7.Only in List A: {onlyin_oneset(list_a,list_b)}')
+        logging.info(f'8.List A is sublist of List B : {subset(list_a,list_b)}')
+        logging.info(f'9.Frequency Count : {dict_frequency(freq_list)}')
+        logging.info(f'10.First Unique Element : {dict_first_unique(freq_list)}')
+        logging.info(f'11.Frequency Group : {dict_freq_grp(freq_list)}')
+        logging.info(f'12.Merging Dictionary {dict_merge(dict1,dict2)}')
+    except Exception as e:
+        logging.exception(f'Exception : {e}')
+        
+    
+if __name__=='__main__':
+    main()
